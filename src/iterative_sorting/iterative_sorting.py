38c7b407fe8d7bdd7f7d3045f365bbe0702b39be
John O'Keefe <jokeefe@weegoglobal.com>
2019-12-04 23:39:34 -0500

    finished interative sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,42 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+l = [ 8, 4, 2, 6, 7, 0]
 
-
-
-        # TO-DO: swap
-
-
-
+# TO-DO: implement the Insertion Sort function below
+def insertion_sort( arr ):
+    for index in range(1, len(arr)):
+        while index > 0 and arr[index] < arr[index -1]:
+            arr[index], arr[index - 1] = arr[index - 1], arr[index]
+            index -= 1
+            print(arr)
 
     return arr
 
+# TO-DO: Complete the selection_sort() function below 
+def selection_sort( arr ):
+    spot_marker = 0
+    while spot_marker <= len(arr):
+        for index in range(spot_marker, len(arr)):
+            if arr[index] < arr[spot_marker]:
+                arr[index], arr[spot_marker] = arr[spot_marker], arr[index]
+                print(arr)
+        spot_marker += 1
+
+    return arr
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    swap_happened = True
+    while swap_happened:
+        swap_happened = False
+        for index in range(len(arr) -1):
+            if arr[index] > arr[index + 1]:
+                swap_happened = True
+                arr[index], arr[index + 1] = arr[index + 1], arr[index]
+                print(arr)
     return arr
-
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+
